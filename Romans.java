8ee3aae95e74c922e2ae7c771d7b52cf639f7b7f
Viv <harina2@cherrycreekschools.org>
2024-09-19 19:28:46 +0000

    committing ^^

--- Romans.java
+++ Romans.java
@@ -12,12 +12,24 @@ public class Romans
 	{
 		m = new TreeMap<String, Integer>();
 		
-		//complete your map				
+		m.put("I", 1);
+		m.put("V", 5);
+		m.put("X", 10);
+		m.put("L", 50);
+		m.put("C", 100);
+		m.put("D", 500);
+		m.put("M", 1000);			
 	}
 	
 	public int getNumber( String s )
 	{
-		//complete this method
+		return m.get(s);
 	}
+	public static void main(String[]args) {
+		Romans romans = new Romans();
 
+		System.out.println(romans.getNumber("V"));
+		System.out.println(romans.getNumber("D"));
+		System.out.println(romans.getNumber("M"));
+	}
 }
